9a747b67f6c1b3ca2eff445c4f7b40d721a86498
Godot-B <bae0821@gmail.com>
2025-05-17 18:02:58 +0900

    client read 커맨드 핸들링 구현 완료

--- ShareDocsClient/src/ShareDocsClient.java
+++ ShareDocsClient/src/ShareDocsClient.java
@@ -5,28 +5,93 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ShareDocsClient {
+
+    private static BufferedReader in;
+    private static PrintWriter out;
+
     public static void main(String[] args) throws IOException {
         String ServerIP = "localhost";
         int port = 12345;
-        Socket serverSocket = new Socket(ServerIP, port);
+        Socket socket = new Socket(ServerIP, port);
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         // PrintWriter는 버퍼링이 있으므로 autoFlush 진리값을 true로 설정
-        PrintWriter out = new PrintWriter(serverSocket.getOutputStream(), true);
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        System.out.println(getSingleResponse());  // 환영 메시지 & 안내 response
 
         BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
         String userLine;
-        System.out.println("메시지를 입력하세요 (bye 입력 시 종료):");
+
         while ((userLine = userInput.readLine()) != null) {
+
             out.println(userLine);
-            String response = in.readLine();
-            System.out.println("서버 응답: " + response);
+
+            String[] tokens = userLine.split(" ");
+            String command = tokens[0];
+            switch (command) {
+                case "write":
+                    handleWrite(tokens);
+                    break;
+                case "read":
+                    handleRead(tokens);
+                    break;
+                default:
+                    System.out.println(getSingleResponse());
+            }
+
             if (userLine.equalsIgnoreCase("bye")) break;
         }
 
         in.close();
         out.close();
-        serverSocket.close();
+        socket.close();
         System.out.println("클라이언트 종료.");
+    }
+
+    private static void handleWrite(String[] tokens) {
+        // TODO 0. 대기 여부 처리
+        // TODO 1. 클라이언트가 user의 편리한 입력 인터페이스 제공
+        // TODO 2. 64KB 줄 단위 out.println, 끝을 알리는 __END__를 붙임
+    }
+
+    private static void handleRead(String[] tokens) throws IOException {
+        // read
+        if (tokens.length == 1) {
+            String line;
+            while ((line = in.readLine()) != null && !line.equals("__END__")) {
+                System.out.println(line);  // 문서 제목 출력
+
+                while ((line = in.readLine()) != null && !line.equals("__SEP__")) {
+                    if (line.equals("__END__")) break;
+                    System.out.println("\t" + line);  // 섹션 제목 출력
+                }
+            }
+
+            return;
+        }
+
+        String response = getSingleResponse();
+        // read <d_title> <s_title>>
+        if (response.equals(tokens[1])) {
+            System.out.println(response);  // 문서 제목 출력
+            String line;
+            System.out.println("\t" + getSingleResponse());  // 섹션 제목 출력
+            while ((line = in.readLine()) != null && !line.equals("__END__")) { // 섹션 내용 출력
+                System.out.println("\t" + "\t" + line);
+            }
+        } else {  // 잘못된 명령어 - 서버의 에러 메시지 출력
+            System.out.println(response);
+        }
+    }
+
+    private static String getSingleResponse() {
+        String response = "";
+        try {
+            response = in.readLine();
+        } catch (IOException e) {
+            System.err.println("서버로부터 수신 실패: " + e.getMessage());
+        }
+        return response;
     }
 }
